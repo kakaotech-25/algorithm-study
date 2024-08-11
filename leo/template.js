0e5aadec9e900942cb96ab99dad3a4862494bb8d
JaeJunday <wowns8114@gmail.com>
2024-08-11 20:35:28 +0900

    template

---
+++ leo/template.js
@@ -0,0 +1,20 @@
+/**
+ *  제목 : 
+ *  입력 :
+ *  제한사항 :
+ *  시간계산 필요 :
+ */
+
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+	let rl = readline.createInterface({ input: process.stdin });
+	
+	for await (const line of rl) {
+		console.log('Hello Goorm! Your input is', line);
+		rl.close();
+	}
+	
+	process.exit();
+})();
