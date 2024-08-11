7f3f4c8ffd499bba7206e436c139fd9b1d0e7cfc
msung99 <msung6924@naver.com>
2024-08-11 19:18:59 +0900

    2559

---
+++ haon/1주차 문자열, 누적합, 구현/tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int sum[10002];
+int ans=-1999999999;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> k;
+
+    for(int i=1; i<=n; i++) {
+        int data;
+        cin >> data;
+        sum[i] = sum[i-1] + data;
+    }
+
+    for(int i=k; i<=n; i++) {
+        ans = max(ans, sum[i] - sum[i-k]);
+    }
+    cout << ans;
+
+    return 0;
+}
