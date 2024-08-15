e0fcf7a8654be118c099b2deb89abb2b827de556
1013115 <108274782+1013115@users.noreply.github.com>
2024-08-15 20:52:53 +0900

    Solved M-3986.py

---
+++ mindy/week1/M-3986.py
@@ -0,0 +1,22 @@
+# 스택 사용
+# ABBA[AB][ABB][A],[AA]
+
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+cnt = 0
+for _ in range(n):
+    alpa = input().strip()
+    stack=[]
+    for i in alpa:
+        if stack and stack[-1] ==i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if not stack:
+        cnt +=1
+
+print(cnt)
