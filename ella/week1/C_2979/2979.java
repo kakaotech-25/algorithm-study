c61b2131e385dad404d79c463cc1fb6df230d1a4
rimeir <128125865+rimeir@users.noreply.github.com>
2024-08-18 22:47:25 +0900

    Create 2979.java

---
+++ ella/week1/C_2979/2979.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int[] time = new int[100]; //시간에 따른 자동차 개수를 나타내는 배열 생성
+        int cost = 0;
+
+        for(int i=0; i<3; i++){
+            int timeIn = sc.nextInt();
+            int timeOut = sc.nextInt();
+            for(int j=timeIn; j<timeOut; j++){
+                time[j]++;
+            }
+        }
+
+        for(int i=0; i<time.length; i++){
+            if(time[i] == 1) cost += a;
+            else if(time[i] == 2) cost += (b * 2);
+            else if(time[i] == 3) cost += (c * 3);
+        }
+        System.out.print(cost);
+    }
+}
