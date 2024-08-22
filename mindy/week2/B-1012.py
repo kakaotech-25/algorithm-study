068bfab4c5eb45dc85c50cfcab0776815ad21134
1013115 <108274782+1013115@users.noreply.github.com>
2024-08-22 22:03:03 +0900

    Update B-1012.py

--- mindy/week2/B-1012.py
+++ mindy/week2/B-1012.py
@@ -8,8 +8,9 @@ dy = [0,0,-1,1]
 def bfs(graph,x,y):
     queue = deque()
     queue.append((x,y))
-    graph[x][y] = 0 
+    graph[x][y] = 0 # 시작점을 방문 했다고 친다 
     
+    # 인접한 모든 1을 탐색하면서 0으로 변경
     while queue:
         x,y = queue.popleft()
         for i in range(4):
@@ -23,6 +24,7 @@ def bfs(graph,x,y):
                 queue.append((nx,ny))
     return
 
+# k개의 좌표를 받아 그 위치들을 다 1로 지정
 for _ in range(t):
     m, n, k = map(int, input().split())
     graph =[[0]*(n) for _ in range(m)]
@@ -31,6 +33,8 @@ for _ in range(t):
         x, y = map(int,input().split())
         graph[x][y]=1
     cnt=0
+    # 2중 for문으로 모든 위치 탐색
+    # 모든 좌표를 탐색하며 1인 경우 0으로 변경
     for a in range(m):
         for b in range(n):
             if graph[a][b]==1:
