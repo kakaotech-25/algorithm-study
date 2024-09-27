26b18851cb0c081d7ba098f3a3a0e45aae7880c0
rimeir <skfsk1602@gmail.com>
2024-09-28 00:38:41 +0900

    week6 1149.py solved

--- ella/week6/F_1149.py
+++ ella/week6/F_1149.py
@@ -1,4 +1,3 @@
-#unsolved
 #백로그
 import sys
 input = sys.stdin.readline
@@ -10,11 +9,13 @@ for i in range(n):
 
 def sum(n):
     dp = [[0]*3 for _ in range(n)]
+    dp[0] = col[0] #첫행 설정
 
+    # 이전 열에 같은 색상이 아닌 값 중 작은 값 + 현재 열 값
     for i in range(1, n):
         dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + col[i][0]
         dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + col[i][1]
         dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + col[i][2]
-    return min(dp[n-1])
+    return min(dp[n-1]) # 총 합이 가장 작은 값 리턴
 
 print(sum(n))
