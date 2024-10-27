8db5015c084552812784ed3f2949cdd095b4cc01
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-10-27 19:32:10 +0900

    solved

---
+++ lian/week11/1-15664.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(N, M, start, path):
+    if len(path) == M:
+        print(' '.join(map(str, path)))
+        return
+    tmp = 0
+    for i in range(start, N):
+        if tmp != nums[i]:
+            dfs(N, M, i + 1, path + [nums[i]])
+            tmp = nums[i]
+
+N, M = map(int, input().split())
+nums = sorted(map(int, input().split()))
+dfs(N, M, 0, [])
