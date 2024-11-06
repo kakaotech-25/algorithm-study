5766a62d12ba21b276880ebb4068459432d4eb70
Do-yoon <1.kim.doyoon@gmail.com>
2024-11-06 15:51:24 +0900

    과자 나눠주기

--- dorothy/week12/4-16401.py
+++ dorothy/week12/4-16401.py
@@ -3,3 +3,26 @@ input = sys.stdin.readline
 
 m, n = map(int, input().strip().split())
 l = tuple(map(int, input().strip().split()))
+
+low = 0
+high = max(l)
+maxlen = 0
+while low <= high:
+    mid = (low + high) // 2
+    # print(f"{high} {mid} {low}")
+    if mid != 0:
+        tmp = sum(map(lambda x: x//mid, l))
+        if mid > 0 and tmp >= m:
+            maxlen = max(maxlen, mid)
+        if tmp >= m:
+            low = mid + 1
+        else:
+            high = mid - 1
+    else:
+        if high > 0 and sum(map(lambda x: x//high, l)) >= m:
+            maxlen = max(maxlen, high)
+        if low > 0 and sum(map(lambda x: x//low, l)) >= m:
+            maxlen = max(maxlen, low)
+        break
+
+print(maxlen)
